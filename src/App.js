8ae827d210dfcaff1e73e7f8af1521efe9e4c361
savyatha <savyathareddy5@gmail.com>
2024-04-15 12:13:12 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import { Component } from "react";
+
+import NxtWatchContext from "./context/NxtWatchContext";
+
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Trending from "./components/Trending";
+import Gaming from "./components/Gaming";
+import VideoItemDetails from "./components/VideoItemDetails";
+import SavedVideos from "./components/SavedVideos";
+import ProtectedRoute from "./components/ProtectedRoute";
+import NotFound from "./components/NotFound";
+import "./App.css";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom/cjs/react-router-dom";
+
+class App extends Component {
+  state = {
+    isDarkTheme: false,
+    savedVideosList: [],
+  };
+
+  toggleTheme = () => {
+    this.setState((prevState) => ({
+      isDarkTheme: !prevState.isDarkTheme,
+    }));
+  };
+
+  toggleSavedListItem = (videoDetails) => {
+    this.setState((prevState) => {
+      if (
+        prevState.savedVideosList.some(
+          (eachVideo) => eachVideo.id === videoDetails.id
+        )
+      ) {
+        return {
+          savedVideosList: prevState.savedVideosList.filter(
+            (eachVideo) => eachVideo.id !== videoDetails.id
+          ),
+        };
+      }
+      return {
+        savedVideosList: [...prevState.savedVideosList, videoDetails],
+      };
+    });
+  };
+
+  render() {
+    const { isDarkTheme, savedVideosList } = this.state;
+
+    return (
+      <NxtWatchContext.Provider
+        value={{
+          isDarkTheme,
+          toggleTheme: this.toggleTheme,
+          savedVideosList,
+          toggleSavedListItem: this.toggleSavedListItem,
+        }}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/login" component={Login} />
+            <ProtectedRoute exact path="/" component={Home} />
+            <ProtectedRoute exact path="/trending" component={Trending} />
+            <ProtectedRoute exact path="/gaming" component={Gaming} />
+            <ProtectedRoute
+              exact
+              path="/saved-videos"
+              component={SavedVideos}
+            />
+            <ProtectedRoute
+              exact
+              path="/videos/:id"
+              component={VideoItemDetails}
+            />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect to="/not-found" />
+          </Switch>
+        </BrowserRouter>
+      </NxtWatchContext.Provider>
+    );
+  }
+}
+
+export default App;
